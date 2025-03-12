3a2d409754359b71fcd76645a532eac3234150b7
Leogaltre <Leogaltre@gmail.com>
2025-03-11 22:51:42 -0600

    React | Modification on ExpenseItem.jsx | Including style

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const date = new Date(2025, 3, 8);
+    const title = Libros;
+    const amount = 250;
+    
     return(
-        <div>
+        <div className='expense-item'>
             <div>Marzo 8 2025</div>
-            <div>
+            <div className='expense-item-descrition'>
                 <h2>Libros</h2>
-                <div>$250</div>
+                <div className='expense-item-price'>$250</div>
             </div>
         </div>
     );
