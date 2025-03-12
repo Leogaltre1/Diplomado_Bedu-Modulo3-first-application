4a79a2ef0c7a3ae8a099e33ca4448098fb2bf5a6
Leogaltre <Leogaltre@gmail.com>
2025-03-11 22:49:25 -0600

    React | Include components/ExpenseItem | First try

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>Hola Mundo</div>
+      <ExpenseItem/>
+    </div> //Solo se puede retornar un solo componente
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return(
+        <div>
+            <div>Marzo 8 2025</div>
+            <div>
+                <h2>Libros</h2>
+                <div>$250</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem //Esto significa que se usara en todos lados
