dbc2675c5ab2ee5172df4a1986962e89e561839c
Leogaltre <Leogaltre@gmail.com>
2025-03-11 23:31:49 -0600

    React | Modification on App.jsx & ExpenseItem.jsx | Including props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,88 @@ import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
+  const expenses = [
+    {
+      id: Math.random(),
+      date: new Date(2022, 4, 23),
+      title: "Libros",
+      amount: 250
+    },
+    {
+      id: Math.random(),
+      date: new Date(2022, 2, 20),
+      title: "Cafe",
+      amount: 50
+    },
+    {
+      id: Math.random(),
+      date: new Date(2022, 3, 18),
+      title: "Comida",
+      amount: 600
+    }
+  ];
+
   return (
     <div>
       <div>Hola Mundo</div>
-      <ExpenseItem/>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
     </div> //Solo se puede retornar un solo componente
   )
 }
 
 export default App
+
+
+/*
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import ExpenseItem from "./components/ExpenseItem"
+
+function App() {
+  const expenses = [
+    {
+      id: Math.random(),
+      date: new Date(2022, 4, 23),
+      title: "Libros",
+      amount: 250
+    },
+    {
+      id: Math.random(),
+      date: new Date(2022, 2, 20),
+      title: "Cafe",
+      amount: 50
+    },
+    {
+      id: Math.random(),
+      date: new Date(2022, 3, 18),
+      title: "Comida",
+      amount: 600
+    }
+  ];
+  return (
+    <div>
+      <div>Hola Mundo</div>
+      
+    </div>
+  )
+}
+
+export default App
+*/
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const date = new Date(2025, 3, 8);
-    const title = "Libros";
-    const amount = 250;
+function ExpenseItem(props) {
     
     return(
         <div className='expense-item'>
-            <div>{date.toLocaleDateString()}</div>
+            <div>{props.date.toLocaleDateString()}</div>
             <div className='expense-item-descrition'>
-                <h2>{title}</h2>
-                <div className='expense-item-price'>${amount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item-price'>${props.amount}</div>
             </div>
         </div>
     );
