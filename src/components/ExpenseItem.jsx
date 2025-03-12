5af184ac3a08983c099cef35bbe2feee7b9600c2
Leogaltre <Leogaltre@gmail.com>
2025-03-11 23:08:02 -0600

    React | Modification on ExpenseItem.jsx | Including const in html code for return

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,15 +2,15 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
     const date = new Date(2025, 3, 8);
-    const title = Libros;
+    const title = "Libros";
     const amount = 250;
     
     return(
         <div className='expense-item'>
-            <div>Marzo 8 2025</div>
+            <div>{date.toLocaleDateString()}</div>
             <div className='expense-item-descrition'>
-                <h2>Libros</h2>
-                <div className='expense-item-price'>$250</div>
+                <h2>{title}</h2>
+                <div className='expense-item-price'>${amount}</div>
             </div>
         </div>
     );
